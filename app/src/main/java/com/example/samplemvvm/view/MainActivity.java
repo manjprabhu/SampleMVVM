60684e9451b88635d65fa00c774486f3a01de8e3
btkmnj <30796192+manjprabhu@users.noreply.github.com>
2019-04-09 16:20:10 +0530

    Retrofit initial commit

--- app/src/main/java/com/example/samplemvvm/view/MainActivity.java
+++ app/src/main/java/com/example/samplemvvm/view/MainActivity.java
@@ -1,20 +1,38 @@
-package com.example.samplemvvm;
+package com.example.samplemvvm.view;
 
+import android.arch.lifecycle.ViewModelProvider;
+import android.arch.lifecycle.ViewModelProviders;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewAnimationUtils;
+
+import com.example.samplemvvm.R;
+import com.example.samplemvvm.data.RetrofitClient;
+import com.example.samplemvvm.data.RetrofitRequest;
+import com.example.samplemvvm.model.Response;
+import com.example.samplemvvm.viewmodel.MainActivityModel;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
 
 public class MainActivity extends AppCompatActivity {
+
+    MainActivityModel mainActivityModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mainActivityModel = ViewModelProviders.of(this).get(MainActivityModel.class);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -26,7 +44,11 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mainActivityModel.initRequest();
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
