7b37d0e3eefa7b9dd3b7298ee90c0190fb1b2c20
btkmnj <30796192+manjprabhu@users.noreply.github.com>
2019-04-09 16:25:01 +0530

    Retrofit initial commit

---
+++ app/src/main/java/com/example/samplemvvm/viewmodel/MainActivityModel.java
@@ -0,0 +1,36 @@
+package com.example.samplemvvm.viewmodel;
+
+import android.arch.lifecycle.ViewModel;
+import android.util.Log;
+
+import com.example.samplemvvm.data.RetrofitClient;
+import com.example.samplemvvm.data.RetrofitRequest;
+import com.example.samplemvvm.model.Response;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class MainActivityModel extends ViewModel {
+
+    private final String TAG = MainActivityModel.class.getSimpleName();
+
+    public void initRequest() {
+
+        RetrofitRequest request = RetrofitClient.getRetrofitClient().create(RetrofitRequest.class);
+
+        request.getResponse().enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                Log.v(TAG,"retrofit response model success");
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+                Log.v(TAG,"retrofit response model failure-->"+t.toString());
+            }
+        });
+
+    }
+
+
+}
